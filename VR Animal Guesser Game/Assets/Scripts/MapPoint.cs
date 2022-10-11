aecf427cbcdda6025035a2226636dac561fa6a88
RoyalTartToter <70082153+RoyalTartToter@users.noreply.github.com>
2022-10-11 14:32:27 +1030

    StreetView Materials
    Collects streetview data in real time

---
+++ VR Animal Guesser Game/Assets/Scripts/MapPoint.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPoint : MonoBehaviour
+{
+    // maintain a reference to the material
+    Material m_material;
+
+    //GameObject m_animal_entered = null;
+    string m_animal_entered;
+
+    // different colours for the point
+    Color m_red = new Color(1.0f, 0.2f, 0.2f, 1.0f);
+    Color m_green = new Color(0.2f, 1.0f, 0.2f, 1.0f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize the material
+        m_material = GetComponent<Renderer>().material;
+        m_material.color = m_red;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if the user brings an animal model in range of the map point
+        if (other.tag == "Animal")
+        {
+            // change the colour of the map point
+            m_material.color = m_green;
+            Debug.Log(other.name);
+
+            // track whether the animal stays inside the trigger
+            m_animal_entered = other.name;
+
+            Debug.Log(m_animal_entered);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        // if the user brings an animal model back out of range of the map point
+        if (other.tag == "Animal")
+        {
+            // change the colour of the map point
+            m_material.color = m_red;
+            //Debug.Log(other.name);
+
+            //m_animal_entered = null;
+        }
+    }
+
+    
+    public void OnAnimalDrop(GameObject dropped_animal)
+    {
+        Debug.Log(dropped_animal.name + "HHHHH");
+        Debug.Log(m_animal_entered + "DDDDD");
+
+        GameObject animal = GameObject.Find(m_animal_entered);
+        // if the dropped animal matches the animal currently entered the trigger, then snap the animal to the map point
+        if (animal != null && GameObject.ReferenceEquals(animal, dropped_animal))
+        {
+            // add this animal to the list of animals currently belonging to this point
+
+            // remove the animal's rigid body
+            Destroy(dropped_animal.GetComponent<Rigidbody>());
+
+            // snap the animal to this point
+            dropped_animal.transform.position = gameObject.transform.position;
+            dropped_animal.transform.SetParent(gameObject.transform);
+        }
+    }
+
+}
---
+++ VR Animal Guesser Game/Assets/Scripts/StreetViewImageLoader.cs
@@ -0,0 +1,104 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StreetViewImageLoader : MonoBehaviour
+{
+    public Material material;
+    [SerializeField]
+    private double heading = 0.0;
+    [SerializeField]
+    private double pitch = 0.0;
+
+    [SerializeField]
+    private int width = 2048;
+    [SerializeField]
+    private int height = 2048;
+
+    [SerializeField]
+    private double longitude = 139.667431;
+    [SerializeField]
+    private double latitude = 35.697408;
+    [SerializeField]
+    private Texture2D frontTex, leftTex, rightTex, backTex, upTex, downTex;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    public void CreateStreetView()
+    {
+        StreetView(latitude, longitude, pitch, heading);
+    }
+
+    public void StreetView(double latitude, double longitude, double pitch, double heading)
+    {
+        StartCoroutine(GetStreetViewImage(latitude, longitude, 0, pitch));
+        StartCoroutine(GetStreetViewImage(latitude, longitude, 90, pitch));
+        StartCoroutine(GetStreetViewImage(latitude, longitude, 180, pitch));
+        StartCoroutine(GetStreetViewImage(latitude, longitude, 270, pitch));
+        StartCoroutine(GetStreetViewImage(latitude, longitude, heading, 90));
+        StartCoroutine(GetStreetViewImage(latitude, longitude, heading, -90));
+        StartCoroutine(WaitTime());
+    }
+
+    public void SetSkybox(Material material)
+    {
+        RenderSettings.skybox = material;
+    }
+
+    public Material setMaterial()
+    {
+        material.SetTexture("_FrontTex", frontTex);
+        material.SetTexture("_BackTex", backTex);
+        material.SetTexture("_LeftTex", leftTex);
+        material.SetTexture("_RightTex", rightTex);
+        material.SetTexture("_UpTex", upTex);
+        material.SetTexture("_DownTex", downTex);
+        return material;
+    }
+    public IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(1f);
+        Material material = setMaterial();
+        SetSkybox(material);
+    }
+
+    public IEnumerator GetStreetViewImage(double latitude, double longitude, double heading, double pitch)
+    {
+        string url = "http://maps.googleapis.com/maps/api/streetview?" + "key=AIzaSyCG8IZe9C0weV3WFGZtrQIRF-p14gFwJPY" + "&size=" + width + "x" + height + "&location=" + latitude + "," + longitude + "&heading=" + heading + "&pitch=" + pitch + "&fov=90&sensor=false";
+
+        WWW www = new WWW(url);
+        yield return www;
+        www.texture.wrapMode = TextureWrapMode.Clamp;
+        if ((int)heading == 0)
+        {
+            if ((int)pitch == 0)
+            {
+                frontTex = www.texture;
+            }
+            else if ((int)pitch == 90)
+            {
+                upTex = www.texture;
+            }
+            else if ((int)pitch == -90)
+            {
+                downTex = www.texture;
+            }
+        }
+        else if ((int)heading == 90)
+        {
+            leftTex = www.texture;
+        }
+        else if ((int)heading == 180)
+        {
+            backTex = www.texture;
+        }
+        else if ((int)heading == 270)
+        {
+            rightTex = www.texture;
+        }
+        Debug.Log(www.texture);
+    }
+}
